4e17fd93b1e080e23c6ac215efe2be8b9c02a1e3
madhu <madhu@madhura_14>
2025-07-06 13:47:05 +0530

    day6

---
+++ day6/src/com/tns/StaticDemo/EmployeeDemo.java
@@ -0,0 +1,17 @@
+package com.tns.StaticDemo;
+
+
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		
+		//Creating first object of the class 
+		
+		Employee e=new Employee("Shubham", 1001);
+		System.out.println(e);
+
+	}
+
+}
