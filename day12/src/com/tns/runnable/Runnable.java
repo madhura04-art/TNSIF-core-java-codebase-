fcbcb573d0d51f01c2414a5d4cf46fed988762d0
madhu <madhu@madhura_14>
2025-07-13 17:44:27 +0530

    day12

---
+++ day12/src/com/tns/runnable/Runnable.java
@@ -0,0 +1,12 @@
+package com.tns.runnable;
+
+public class Runnable {
+	int i;
+	String Str;
+  public Runnable(int i,String str) {
+	  
+	  this.i= i;
+	  this.Str=str;
+  }
+  
+}
---
+++ day12/src/com/tns/runnable/runnableDemo.java
@@ -0,0 +1,13 @@
+package com.tns.runnable;
+
+public class runnableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Runnable r = new Runnable(1,"hi");
+      Thread t = new Thread();
+      t.start();
+      
+	}
+
+}
